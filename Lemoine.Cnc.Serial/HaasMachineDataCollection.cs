779155d2357ccf2f4389731cb8f43ddba46ae17f
Nicolas Relange <nrelange@lemoinetechnologies.com>
2023-04-05 11:26:36 +0200

    Fix the logs with exception, request ThreeInOne only once, fix ProgramName when the there is no space after the comma, parts from ThreeInOne

--- Lemoine.Cnc.Serial/HaasMachineDataCollection.cs
+++ Lemoine.Cnc.Serial/HaasMachineDataCollection.cs
@@ -19,12 +19,14 @@ namespace Lemoine.Cnc
   /// <item>Handshake: XOn/XOff</item>
   /// <item>Data bits: 8</item>
   /// </summary>
-  public class HaasMachineDataCollection: AbstractSerial, Lemoine.Cnc.ICncModule
+  public class HaasMachineDataCollection : AbstractSerial, Lemoine.Cnc.ICncModule
   {
     #region Members
     bool m_error = false;
     string m_previousMotionTime = null;
     bool m_echo = true;
+    string m_threeInOne = null;
+    bool m_threeInOneRequested = false;
     #endregion
 
     #region Getters / Setters
@@ -38,38 +40,80 @@ namespace Lemoine.Cnc
       get { return m_echo; }
       set { m_echo = value; }
     }
-    
+
     /// <summary>
     /// Error ?
     /// </summary>
-    public bool Error {
+    public bool Error
+    {
       get { return m_error; }
     }
-    
+
     /// <summary>
     /// Is the machine running ?
     /// 
     /// Get this property from the motion time
     /// </summary>
-    public bool Running {
+    public bool Running
+    {
       get { return GetRunningFromMotionTime (""); }
     }
-    
+
+    /// <summary>
+    /// Three in one
+    /// 
+    /// This is only known when the machine is idle
+    /// </summary>
+    public string ThreeInOne
+    {
+      get {
+        if (m_threeInOne is null) {
+          if (m_threeInOneRequested) {
+            if (log.IsDebugEnabled) {
+              log.Debug ($"ThreeInOne: already requested");
+            }
+            throw new Exception ("ThreeInOne: already requested");
+          }
+          var q500 = GetString ("Q500");
+          m_threeInOneRequested = true;
+          if (q500.StartsWith ("PROGRAM")) {
+            m_threeInOne = q500;
+          }
+          else {
+            if (log.IsDebugEnabled) {
+              log.Debug ($"ThreeInOne: busy, {q500}");
+            }
+            throw new Exception ("ThreeInOne: busy");
+          }
+        }
+        return m_threeInOne;
+      }
+    }
+
     /// <summary>
     /// Program name
     /// 
     /// This is only known when the machine is idle
     /// </summary>
-    public string ProgramName {
-      get
-      {
-        string threeInOne = GetString ("Q500");
-        if (threeInOne.StartsWith ("PROGRAM")) {
-          return threeInOne.Split (new string[] { ", " }, StringSplitOptions.None) [1];
+    public string ProgramName => this.ThreeInOne.Split (new string[] { ", ", "," }, StringSplitOptions.None)[1];
+
+    /// <summary>
+    /// Part count from ThreeInOne
+    /// </summary>
+    public string ThreeInOneParts {
+      get {
+        var threeInOne = this.ThreeInOne;
+        var partsPosition = threeInOne.IndexOf ("PARTS,");
+        if (-1 == partsPosition) {
+          log.Error ($"ThreeInOneParts: no PARTS in {threeInOne}");
+          throw new Exception ("No PARTS in three in one");
         }
-        else {
-          throw new Exception ("ProgramName: busy");
+        var subString = threeInOne.Substring (partsPosition + "PARTS,".Length).Trim ();
+        var parts = subString.Split (new string[] { ", ", "," }, StringSplitOptions.None)[0];
+        if (log.IsDebugEnabled) {
+          log.Debug ($"ThreeInOneParts: return {parts} from {threeInOne}");
         }
+        return parts;
       }
     }
     #endregion
@@ -95,7 +139,7 @@ namespace Lemoine.Cnc
       this.Handshake = "XOnXOff";
       this.DataBits = 8;
     }
-    
+
     // Note: the Dispose method is implemented in
     //       the base class AbstractSerial
     #endregion
@@ -116,66 +160,57 @@ namespace Lemoine.Cnc
                         request,
                         this.SerialPort.PortName);
         try {
-          SerialPort.Open();
+          SerialPort.Open ();
         }
         catch (Exception ex) {
-          log.ErrorFormat ("GetString ({0}): " +
-                           "Open IO COM Exception {1}",
-                           request,
-                           ex.Message);
+          log.Error ($"GetString ({request}): Open IO COM Exception", ex);
           m_error = true;
-          throw ex;
+          throw;
         }
         if (SerialPort.IsOpen == false) {
-          log.FatalFormat ("GetString ({0}): " +
-                           "IO COM is not opened " +
-                           "even after successfully opening the port",
-                           request);
+          log.Fatal ($"GetString ({request}): IO COM is not opened even after successfully opening the port");
           m_error = true;
           throw new Exception ("IO COM is not opened");
         }
       }
 
-      log.DebugFormat ("GetString /B request={0}",
-                       request);
+      if (log.IsDebugEnabled) {
+        log.Debug ($"GetString /B request={request}");
+      }
 
       try {
         SerialPort.WriteLine (request);
-        log.DebugFormat ("GetString ({0}): " +
-                         "request was written",
-                         request, request);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetString ({request}): request was written");
+        }
       }
       catch (Exception ex) {
-        log.ErrorFormat ("GetString ({0}): " +
-                         "Write exception {1}",
-                         request, ex.Message);
+        log.Error ($"GetString ({request}): Write exception", ex);
         SerialPort.Close ();
         m_error = true;
-        throw ex;
+        throw;
       }
-      
+
       string response = "";
       bool requestLineReadIfEcho = !m_echo;
       for (int i = 0; i < 4; i++) { // Try several times to read the right line
-        // If echo is on:
-        // 1st line: request (Qxxx)
-        // 2nd line: response
-        // else:
-        // 1st line: response
+                                    // If echo is on:
+                                    // 1st line: request (Qxxx)
+                                    // 2nd line: response
+                                    // else:
+                                    // 1st line: response
         try {
           response = SerialPort.ReadLine ();
         }
         catch (Exception ex) {
-          log.ErrorFormat ("GetString ({0}): " +
-                           "Read exception {1}",
-                           request, ex.Message);
+          log.Error ($"GetString ({request}): exception in ReadLine", ex);
           SerialPort.Close ();
           m_error = true;
-          throw ex;
+          throw;
         }
-        log.DebugFormat ("GetString ({0}): " +
-                         "Got line {1}",
-                         request, response);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetString ({request}): got line {response}");
+        }
         if (response.Contains ("INVALID NUMBER")) { // Error !
           log.ErrorFormat ("GetString ({0}): " +
                            "got error {1}",
@@ -189,27 +224,24 @@ namespace Lemoine.Cnc
                              request, response);
           }
           catch (Exception ex) {
-            log.ErrorFormat ("GetString ({0}): " +
-                             "Read exception after INVALID NUMBER {1}",
-                             request, ex.Message);
+            log.Error ($"GetString ({request}): read exception after INVALID NUMBER", ex);
             SerialPort.Close ();
-            throw ex;
+            throw;
           }
           throw new Exception ("Invalid number");
         }
         if (!requestLineReadIfEcho) {
           if (response.Contains (request)) { // The request was read
-            log.DebugFormat ("GetString ({0}): " +
-                             "request was read",
-                             request);
+            if (log.IsDebugEnabled) {
+              log.Debug ($"GetString ({request}): request was read");
+            }
             requestLineReadIfEcho = true;
             continue;
           }
           else {
-            log.DebugFormat ("GetString ({0}): " +
-                             "request was not read yet, " +
-                             "try to read another line",
-                             request);
+            if (log.IsDebugEnabled) {
+              log.Debug ($"GetString ({request}): request was not read yet, try to read another line");
+            }
             continue;
           }
         }
@@ -228,26 +260,24 @@ namespace Lemoine.Cnc
               throw new Exception ("invalid response after request output");
             }
             else { // false == m_echo => try another line
-              log.DebugFormat ("GetString ({0}): " +
-                               "no STX=0x02 characher in {1} " +
-                               "=> try to read another line",
-                               request,
-                               response);
+              if (log.IsDebugEnabled) {
+                log.DebugFormat ("GetString ({0}): " +
+                                 "no STX=0x02 characher in {1} " +
+                                 "=> try to read another line",
+                                 request,
+                                 response);
+              }
               continue;
             }
           }
           else { // STX found
-            response = response.Substring (indexOfStx+1); // There should be at least one character after STX, else this is ok to raise an exception
-            response = response.TrimEnd (new char[] {(char)0x17}); // Trim ETP = 0x17 (ctrl-W) at end
-            log.DebugFormat ("GetString ({0}): " +
-                             "Got response {1}",
-                             request, response);
-            
+            response = response.Substring (indexOfStx + 1); // There should be at least one character after STX, else this is ok to raise an exception
+            response = response.TrimEnd (new char[] { (char)0x17 }); // Trim ETP = 0x17 (ctrl-W) at end
+            if (log.IsDebugEnabled) {
+              log.Debug ($"GetString ({request}): Got response {response}");
+            }
             if (response.Equals ("UNKNOWN")) { // Error !
-              log.ErrorFormat ("GetString ({0}): " +
-                               "got error {1}",
-                               request,
-                               response);
+              log.Error ($"GetString ({request}): got error {response}");
               m_error = true;
               throw new Exception ("Unknown command");
             }
@@ -256,7 +286,7 @@ namespace Lemoine.Cnc
           }
         }
       }
-      
+
       // The data was not processed in the loop: the request line or STX was not found
       log.ErrorFormat ("GetString ({0}): " +
                        "request line or STX not found. " +
@@ -265,7 +295,7 @@ namespace Lemoine.Cnc
       m_error = true;
       throw new Exception ("request line or STX not found");
     }
-    
+
     /// <summary>
     /// Make a request to the Machine Data Collection module and get a part of it.
     /// Consider ', ' for the CSV separator.
@@ -278,12 +308,11 @@ namespace Lemoine.Cnc
     /// <returns></returns>
     public string GetSubString (string parameter)
     {
-      string[] parameters = parameter.Split (new char[] {':'}, 2);
+      string[] parameters = parameter.Split (new char[] { ':' }, 2);
       if (1 == parameters.Length) { // The separator is not found, consider here we want the full response
-        log.DebugFormat ("GetSubString ({0}): " +
-                         "no separator ':' " +
-                         "=> consider {0} for the request and return the full string",
-                         parameter);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetSubString ({parameter}): no separator ':' => consider {parameter} for the request and return the full string");
+        }
         return GetString (parameter);
       }
       else if (2 != parameters.Length) {
@@ -293,20 +322,18 @@ namespace Lemoine.Cnc
         throw new ArgumentException ("invalid parameter");
       }
       else { // 2 == parameters.Length
-        string request = parameters [0];
+        string request = parameters[0];
         int position;
         try {
-          position = int.Parse (parameters [1]);
+          position = int.Parse (parameters[1]);
         }
-        catch (Exception) {
-          log.ErrorFormat ("GetSubString ({0}): " +
-                           "invalid column number {1}",
-                           parameter, parameters [1]);
+        catch (Exception ex) {
+          log.Error ($"GetSubString ({parameter}): invalid column number {parameters[1]}", ex);
           throw;
         }
         string response = GetString (request);
-        string[] items = response.Split (new string[] {", "}, StringSplitOptions.None);
-        return items [position];
+        string[] items = response.Split (new string[] { ", ", "," }, StringSplitOptions.None);
+        return items[position].Trim ();
       }
     }
 
@@ -319,7 +346,7 @@ namespace Lemoine.Cnc
     {
       return int.Parse (this.GetSubString (param).Trim ());
     }
-    
+
     /// <summary>
     /// Get a double value and round it to the closest integer
     /// </summary>
@@ -327,9 +354,9 @@ namespace Lemoine.Cnc
     /// <returns></returns>
     public int GetRounded (string param)
     {
-      return (int) Math.Round (GetDouble (param));
+      return (int)Math.Round (GetDouble (param));
     }
-    
+
     /// <summary>
     /// Get the long value of a corresponding request
     /// </summary>
@@ -339,7 +366,7 @@ namespace Lemoine.Cnc
     {
       return long.Parse (this.GetSubString (param).Trim ());
     }
-    
+
     /// <summary>
     /// Get the double value of a corresponding request
     /// </summary>
@@ -351,7 +378,7 @@ namespace Lemoine.Cnc
       return double.Parse (this.GetSubString (param).Trim (),
                            usCultureInfo);
     }
-    
+
     /// <summary>
     /// Check if the machine is running from the motion time
     /// 
@@ -363,34 +390,35 @@ namespace Lemoine.Cnc
     {
       string motionTime = GetSubString ("Q301:1");
       if (null == m_previousMotionTime) {
-        log.DebugFormat ("GetRunningFromMotionTime: " +
-                         "initial value is {0}",
-                         motionTime);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetRunningFromMotionTime: initial value is {motionTime}");
+        }
         m_previousMotionTime = motionTime;
         throw new Exception ("GetRunningFromMotionTime: initialization");
       }
       else if (object.Equals (m_previousMotionTime, motionTime)) {
-        log.DebugFormat ("GetRunningFromMotionTime: " +
-                         "same motion time {0} => return false",
-                         motionTime);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetRunningFromMotionTime: same motion time {motionTime} => return false");
+        }
         return false;
       }
       else {
-        log.DebugFormat ("GetRunningFromMotionTime: " +
-                         "the motion time changed from {0} to {1} " +
-                         "=> return true",
-                         m_previousMotionTime, motionTime);
+        if (log.IsDebugEnabled) {
+          log.Debug ($"GetRunningFromMotionTime: the motion time changed from {m_previousMotionTime} to {motionTime} => return true");
+        }
         m_previousMotionTime = motionTime;
         return true;
       }
     }
-    
+
     /// <summary>
     /// Start method: reset the error property
     /// </summary>
     public void Start ()
     {
       m_error = false;
+      m_threeInOne = null;
+      m_threeInOneRequested = false;
     }
     #endregion
   }
